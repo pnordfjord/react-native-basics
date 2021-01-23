4615723a08d7f3a37a49e12ac508797583f4bd34
Phoebe Kotliar <phoebe.kotliar@gmail.com>
2021-01-23 15:50:24 -0500

    finished basic app

--- App.js
+++ App.js
@@ -1,36 +1,9 @@
 import React from "react";
 import { View } from "react-native";
+import { ViewImageScreen } from "./app/screens/ViewImageScreen";
+import { WelcomeScreen } from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  //return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
